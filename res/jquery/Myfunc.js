5b3483739a63c08192460c1d3e8397a7bb54ca00
fengnan <915786639@qq.com>
2019-01-19 14:56:26 +0800

    ‘调试版本’

---
+++ res/jquery/Myfunc.js
@@ -0,0 +1,76 @@
+// 只有一位数时前面补零
+function parse(month) {
+    if (month < 10) {
+        month = '0' + month;
+    }
+    else {
+        month += "";
+    }
+    return month;
+}
+
+// 获取当前的时间 参数为时间戳
+function getTime(timestamp) {
+    timestamp = parseInt(timestamp);
+    var now = Date.parse(new Date()); //获取当前的时间戳
+    var res;
+    if (now - timestamp < 60000) {
+        res = parseInt((now - timestamp) / 1000) + "秒前";
+    }
+    else if (now - timestamp < 3600000) {
+        res = parseInt((now - timestamp) / 60000) + "分钟前";
+    }
+    else if ((now - timestamp < 3600000 * 24)) {
+        res = parseInt((now - timestamp) / 3600000) + '小时前';
+    }
+    else {
+        var time = new Date(timestamp);
+        res = time.getFullYear() + '-' + parse((time.getMonth() + 1)) + "-" + parse(time.getDate()) + " " + parse(time.getHours()) + ":" + parse(time.getMinutes());
+    }
+    return res;
+}
+
+// 初始化好友动态
+function showMoment(list) {
+    let lists = "";
+    for (let i = 0; i < list.length; i++) {
+        lists = lists + `<div class="item">
+                                <div class="item-box  layer-photos-demo1 layer-photos-demo">
+                                    <div class="publish-details">
+                                        <span class="head-nail" style="background: url('${list[i].headpic}') center center;background-size: 100%"></span>
+                                        <div class="nail-right">
+                                            <span class="nickname">${list[i].nickname}</span>
+                                            <span class="publish-time">发布于：${getTime(list[i].time)}</span>
+                                        </div>
+                                        <span class="info-img like ${(list[i].liked)?'layblog-this':''}"><i class="layui-icon  layui-icon-praise"></i>${list[i].like}</span>
+                                    </div>
+                                    <p>${list[i].text}</p>
+                                    <img src = ${list[i].pic} alt="" style="margin-top: 10px;display:${(list[i].pic)? 'block':'none'}">
+                                </div>
+                                <div class="momentID" style="display: none"> ${list[i].id} </div>
+                            </div>`
+    }
+    return lists;
+};
+
+//初始化留言
+function updateContent(messages){
+    let content = " ";
+    for(let i=0; i<messages.length; i++){ // 10处应该为messages.length
+        content = content +`<div class="info-box">
+                    <div class="info-item">
+                        <img class="info-img" src="${messages[i].headpic}" style="width: 70px;height: 70px;" alt="">
+                        <div class="info-text">
+                            <p class="title count">
+                                <span class="name" style="color:#000000;">${messages[i].nick}</span>
+                                <span class="name" style="margin-left:10px;">${getTime(messages[i].time)}</span>
+                                <span class="info-img like ${(messages[i].liked)?'layblog-this':''}"><i class="layui-icon  layui-icon-praise"></i>${messages[i].like}</span>
+                            </p>
+                            <p class="info-intr">${messages[i].text}</p>
+                        </div>
+                    </div>
+                    <div class="messageID" style="display: none">${messages[i].id}</div>
+                </div>`
+    }
+    $('#LAY-msg-box').html(content);
+}
--- res/static/js/blog.js
+++ res/static/js/blog.js
@@ -1,33 +1,10 @@
 /**
+   @Name：闲言
+   @Author 哐哐哐
+**/
 
- @Name：layui.blog 闲言轻博客模块
- @Author：徐志文
- @License：MIT
- @Site：http://www.layui.com/template/xianyan/
-
- */
 layui.define(['element', 'form', 'laypage', 'jquery', 'laytpl'], function (exports) {
-    var element = layui.element
-        , form = layui.form
-        , laypage = layui.laypage
-        , $ = layui.jquery
-        , laytpl = layui.laytpl;
-
-
-    //start 分页
-    var itemsnum = 5;
-    laypage.render({
-        elem: 'test1' //注意，这里的 test1 是 ID，不用加 # 号
-        , count: itemsnum //数据总数，从服务端得到
-        , theme: '#07A8C5',
-        jump: function (obj,first) {
-            if(!first) {
-                alert('new')
-            }
-        }
-    });
-    // end 分页
-
+    var laypage = layui.laypage, $ = layui.jquery, laytpl = layui.laytpl;
 
     // start 导航显示隐藏
     $("#mobile-nav").on('click', function () {
@@ -35,14 +12,13 @@ layui.define(['element', 'form', 'laypage', 'jquery', 'laytpl'], function (expor
     });
     // end 导航显示隐藏
 
-
-    //start 评论的特效
+    // start 评论的特效
     (function ($) {
         $.extend({
             tipsBox: function (options) {
                 options = $.extend({
                     obj: null,  //jq对象，要在那个html标签上显示
-                    str: "+1",  //字符串，要显示的内容;也可以传一段html，如: "<b style='font-family:Microsoft YaHei;'>+1</b>"
+                    str: "+1",  //要显示的内容，也可以传一段htm
                     startSize: "12px",  //动画开始的文字大小
                     endSize: "30px",    //动画结束的文字大小
                     interval: 600,  //动画时间间隔
@@ -73,6 +49,41 @@ layui.define(['element', 'form', 'laypage', 'jquery', 'laytpl'], function (expor
                     box.remove();
                     options.callback();
                 });
+            },
+            tipsBox2: function (options) {
+                options = $.extend({
+                    obj: null,  //jq对象，要在那个html标签上显示
+                    str: "+1",  //要显示的内容，也可以传一段htm
+                    startSize: "12px",  //动画开始的文字大小
+                    endSize: "30px",    //动画结束的文字大小
+                    interval: 600,  //动画时间间隔
+                    color: "red",    //文字颜色
+                    callback: function () {
+                    }    //回调函数
+                }, options);
+
+                $("body").append("<span class='num'>" + options.str + "</span>");
+
+                var box = $(".num");
+                var left = options.obj.offset().left + options.obj.width() ;
+                var top = options.obj.offset().top - 10;
+                box.css({
+                    "position": "absolute",
+                    "left": left + "px",
+                    "top": top + "px",
+                    "z-index": 9999,
+                    "font-size": options.startSize,
+                    "line-height": options.endSize,
+                    "color": options.color
+                });
+                box.animate({
+                    "font-size": options.endSize,
+                    "opacity": "0",
+                    "top": top - parseInt(options.endSize) + "px"
+                }, options.interval, function () {
+                    box.remove();
+                    options.callback();
+                });
             }
         });
     })($);
@@ -83,7 +94,9 @@ layui.define(['element', 'form', 'laypage', 'jquery', 'laytpl'], function (expor
             prop.find('i').removeClass('niceIn');
         }, 1000);
     }
+    // 评论的特效 end
 
+    //  start 留言点赞
     $(function () {
         $('#LAY-msg-box').delegate('span', 'click', function () {
             if ($(this).hasClass('like')) {
@@ -99,14 +112,14 @@ layui.define(['element', 'form', 'laypage', 'jquery', 'laytpl'], function (expor
                     });
                     niceIn($(this));
                     layer.msg('点赞成功', {
-                        icon:6
-                        , time: 1000
+                        icon:6,
+                        time: 1000
                     })
                 }
                 // 取消赞
                 else {
-                    $(this).removeClass('layblog-this');
                     $(this).html('<i class="layui-icon layui-icon-praise"></i>' + (parseInt($(this).text()) - 1));
+                    $(this).removeClass('layblog-this');
                     $.tipsBox({
                         obj: $(this),
                         str: "-1",
@@ -115,50 +128,41 @@ layui.define(['element', 'form', 'laypage', 'jquery', 'laytpl'], function (expor
                     });
                     niceIn($(this));
                     layer.msg('取消点赞', {
-                        icon: 5
-                        , time: 1000
+                        icon: 5,
+                        time: 1000
                     })
                 }
             }
         });
     });
-    //end 评论的特效
+    //  留言点赞 end
 
-
-    // 留言提交按钮
+    // start 留言提交按钮
     $('#item-btn').on('click', function () {
         var elemCont = $('#LAY-msg-content'), content = elemCont.val();
+        // 从本地获取个人信息
+        var d = JSON.parse(localStorage.getItem('pdata'));
 
         if (content.replace(/\s/g, '') == "") {
             layer.msg('请先输入留言');
             return elemCont.focus();
         }
         else{
-            /*$.ajax({
-                type: 'POST',
-                url: url, // 后端接口
-                data: content,
-                success: function () {
-                    //
-                }
-            });*/
-
-            var view = $('#LAY-msg-tpl').html(),
-                d = JSON.parse(localStorage.getItem('pdata')),   // d是模拟从 localstorage 获取的数据
+            var view = $('#LAY-msg-tpl').html();
                 data = {
                     username: d.nickname,
-                    avatar: d.headpic.substring(d.headpic.indexOf('(') + 1, d.headpic.indexOf(")")),
+                    avatar: d.headpic,
                     praise: 0,
                     content: content
                 };
 
             // 分页的内容重新渲染
-            itemsnum++;
+            itemsnum++;  // 数据总数+1
             laypage.render({
-                elem: 'test1' //注意，这里的 test1 是 ID，不用加 # 号
-                , count: itemsnum //数据总数，从服务端得到
-                , theme: '#07A8C5',
-                jump:function (obj,first) {
+                elem: 'test1',
+                count: itemsnum,
+                theme: '#07A8C5',
+                jump: function (obj,first) {
                     if(!first) {
                         console.log(obj.curr); //获取当前页面
                     }
@@ -170,7 +174,6 @@ layui.define(['element', 'form', 'laypage', 'jquery', 'laytpl'], function (expor
                 $(".info-box:last-child").remove();
             }
 
-
             //模板渲染
             laytpl(view).render(data, function (html) {
                 $('#LAY-msg-box').prepend(html);
@@ -179,112 +182,130 @@ layui.define(['element', 'form', 'laypage', 'jquery', 'laytpl'], function (expor
                     icon: 1
                 })
             });
-
         }
     });
+    // 留言提交按钮 end
 
-    // 上传图片
-    let a,pic,style;
+    // start 动态的点赞
+    $(function () {
+        $('.index-wrap').delegate('span', 'click', function () {
+            if ($(this).hasClass('like')) {
+                if (!($(this).hasClass("layblog-this"))) {
+                    this.text = '已赞';
+                    $(this).addClass('layblog-this');
+                    $(this).html('<i class="layui-icon layui-icon-praise"></i>' + (parseInt($(this).text()) + 1));
+                    $.tipsBox2({
+                        obj: $(this),
+                        str: "+1",
+                        callback: function () {
+                        }
+                    });
+                    niceIn($(this));
+                    layer.msg('点赞成功', {
+                        icon:6,
+                        time: 1000,
+                        offset:['50%','50%']
+                    })
+                }
+                // 取消赞
+                else {
+                    $(this).html('<i class="layui-icon layui-icon-praise"></i>' + (parseInt($(this).text()) - 1));
+                    $(this).removeClass('layblog-this');
+                    $.tipsBox2({
+                        obj: $(this),
+                        str: "-1",
+                        callback: function () {
+                        }
+                    });
+                    niceIn($(this));
+                    layer.msg('取消点赞', {
+                        icon: 5,
+                        time: 1000,
+                        offset:['50%','50%']
+                    })
+                }
+            }
+        });
+    });
+    // end 动态的点赞
+
+    // start 上传动态配图
+    let picFile,picSrc;
     $('#upload').on('change',function () {
         // 获取读到的上传文件
-        a = document.getElementById('upload').files[0];
-        if(a){
-            $('#imagepath').text(a.name);
+        picFile = document.getElementById('upload').files[0];
+        if(picFile){
+            $('#imagepath').text(picFile.name);
             // 将成功上传图标改为显示状态
             $('#suc_upload').css({
                 display: 'block'
             });
             var reader = new FileReader();
-            reader.readAsDataURL(a);
+            reader.readAsDataURL(picFile);
             //监听文件读取结束后事件
             reader.onloadend = function (e) {
                 if(e.target.result){
-                    pic = 'src =' +e.target.result;
-                    style = `style ="display:block;width:60%;margin-top:10px"`;
+                    picSrc = 'src =' +e.target.result;
                 }
             };
         }
         $('#upload').val('');
     });
+    // 上传动态配图 end
 
-    // 发表动态提交按钮
+    // start 发表动态
     $('#submit-btn').on('click',function () {
         var content = $('#LAY-msg-content').val();
         if (content.replace(/\s/g, '') == "") {
-            layer.msg('请先输入动态内容');
+            layer.msg('请先输入动态内容',{
+                offset:['30%','50%']
+            });
             return  $('#LAY-msg-content').focus();
         }
 
-        //转换只有一位数的日期时间等
-        function parse(month) {
-            if(month<10){
-                month ='0'+month;
-            }
-            else {
-                month +="";
-            }
-            return month;
+        if(!picFile){
+            picSrc = '';
         }
 
-        // 获取当前的时间 参数为时间戳
-        function getTime(timestamp) {
+        let newDongtai= {
+            content:content,
+            pic : picSrc
+        };
+        console.log(newDongtai);
+        $.ajax({
+            type: 'POST',
+            xhrFields:{
+                withCredentials:true
+            },
+            url: 'http://192.168.1.105:8080/moment/refresh', // 后端接口
+            data: JSON.stringify(newDongtai),
+            contentType: "application/json",
+            success: function (data) {
+                console.log(data);
+                $('.item').remove();
+                $(showMoment(data.moments)).insertAfter(".fixed");
+                $('#LAY-msg-content').val('');
+                layer.msg('动态发布成功', {
+                    icon: 1,
+                });
+            },
+        });
 
-            var now = Date.parse(new Date()); //获取当前的时间戳
-            var res;
-            if(now - timestamp <60000){
-                res = (now - timestamp)/1000 +"秒前";
-            }
-            else if(now - timestamp <3600000){
-                res = parseInt((now - timestamp)/60000) +"分钟前";
-            }
-            else if((now - timestamp < 3600000*24)){
-                res =  parseInt((now - timestamp) /3600000) +'小时前';
-            }
-            else {
-                var time =new Date(timestamp);
-                res = time.getFullYear()+'-'+ parse((time.getMonth()+1)) +"-"+ parse(time.getDate()) + " " +time.getHours()+":"+time.getMinutes();
-            }
-            return res;
-        }
 
-        if(!a){
-            pic = '';
-            style = `style ="display:none"`
-        }
-        var view = $('#LAY-msg-tpl').html(),
-            d = JSON.parse(localStorage.getItem('pdata')),   // d是模拟从 localstorage 获取的数据
-            data = {
-                username: d.nickname,
-                avatar: d.headpic.substring(d.headpic.indexOf('(') + 1, d.headpic.indexOf(")")),
-                praise: 0,
-                content: content,
-                time:'刚刚', // getTime(时间戳)
-                pic:pic,
-                style:style
-            };
-
+        clearPre();
         // 清除本次上传的内容和提示内容
-        function clear(){
-            a = null;
-            pic = null;
+        function clearPre(){
+            picFile = null;
+            picSrc = null;
             $('#suc_upload').css({
                 display: 'none'
             });
             $('.image-path').text('');
         }
-        clear();
-
-        //模板渲染
-        laytpl(view).render(data, function (html) {
-            $(html).insertAfter(".fixed");
-            $('#LAY-msg-content').val('');
-            layer.msg('动态发布成功', {
-                icon: 1
-            })
-        });
-
     });
+    // 发表动态 end
 
     //输出test接口
     exports('blog', {});
-});  
+});
+
